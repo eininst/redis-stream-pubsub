306d306191fb944c998fe72af902cb8d14d06797
王梓清 <9464553+superwzq1993@user.noreply.gitee.com>
2024-12-20 16:52:43 +0800

    clean

--- example/consumer.go
+++ example/consumer.go
@@ -3,11 +3,9 @@ package main
 import (
 	"github.com/eininst/flog"
 	"github.com/eininst/redis-stream-pubsub/pubsub"
-	"os"
 )
 
 func main() {
-	flog.Info(os.Getpid())
 	cs := pubsub.NewConsumer("redis://localhost:6379/0")
 
 	cs.Handler("test", func(ctx *pubsub.Context) error {
