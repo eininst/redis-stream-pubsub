196830c38d9f170117d7c189b887c7ee6cf4d347
王梓清 <9464553+superwzq1993@user.noreply.gitee.com>
2024-12-21 03:06:49 +0800

    init

--- example/consumer.go
+++ example/consumer.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/eininst/redis-stream-pubsub/pubsub"
 	"log"
+	"syscall"
 	"time"
 )
 
 func main() {
 	cs := pubsub.NewConsumer("redis://localhost:6379/0",
-		pubsub.WithTimeout(time.Second*5))
+		pubsub.WithTimeout(time.Second*5),
+		pubsub.WithSignal(syscall.SIGTERM, syscall.SIGINT))
 
 	cs.Handler("test", func(ctx *pubsub.Context) error {
 		log.Printf("received test msg:%v", ctx.Payload)
