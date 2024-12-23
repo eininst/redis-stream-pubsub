a1c1006e26f63f6abe72b493480f222a0bf849b8
王梓清 <9464553+superwzq1993@user.noreply.gitee.com>
2024-12-23 20:51:20 +0800

    fix

--- example/consumer.go
+++ example/consumer.go
@@ -11,12 +11,10 @@ func main() {
 	cs := pubsub.NewConsumer("redis://localhost:6379/0",
 		pubsub.WithTimeout(time.Second*10),
 		pubsub.WithExitWaitTime(time.Second*3),
-		//pubsub.WithNoAck(false),
 		pubsub.WithSignal(syscall.SIGTERM, syscall.SIGINT))
 
 	cs.Handler("test", func(ctx *pubsub.Context) error {
 		log.Printf("received test msg:%v", ctx.Payload)
-		time.Sleep(time.Second * 3)
 		return nil
 	})
 
